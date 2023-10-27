fbb1248558bc894064fcea787dad8c89a0d9f5d5
Bablu12072001 <bkumar41366@gmail.com>
2023-10-27 10:45:39 +0530

    add our sponsers

---
+++ www.algorand.foundation/function.js
@@ -0,0 +1,25 @@
+// $(document).ready(function () {
+
+//   $(".nav-item").click(function (){
+//     $(this).addClass("active").siblings().removeClass("active");
+//   });
+// });
+document.addEventListener("DOMContentLoaded", function () {
+  const selector = ".nav__link";
+  const elems = Array.from(document.querySelectorAll(selector));
+  const navigation = document.querySelector("navbar-nav");
+
+  function makeActive(evt) {
+    const target = evt.target;
+
+    if (!target || !target.matches(selector)) {
+      return;
+    }
+
+    elems.forEach((elem) => elem.classList.remove("active"));
+
+    evt.target.classList.add("active");
+  }
+
+  navigation.addEventListener("mousedown", makeActive);
+});
